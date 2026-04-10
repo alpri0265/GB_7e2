3c563036b1c82c9d0af9bb442bfa8d115bc56a86
alpri0265 <alpri0265@gmail.com>
2026-04-10 19:52:26 +0300

    Запуск
    Сконфігуровані піни в Cube MX

--- stm32/els_f407_black/Core/Inc/app_main.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-void app_init(void);
-void app_loop(void);
-
--- stm32/els_f407_black/Core/Inc/bsp_uart.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stddef.h>
-
-void bsp_uart_init(void);
-void bsp_uart_write(const char *s);
-void bsp_uart_printf(const char *fmt, ...);
-
